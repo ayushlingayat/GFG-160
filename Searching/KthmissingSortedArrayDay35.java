ef5068a2bf2e9c8044f56d8fa5bff533bcb8f0b5
Ayush Shyam Lingayat <lingayatayush3@gmail.com>
2024-12-19 19:16:46 +0530

    Searching...

---
+++ Searching/KthmissingSortedArrayDay35.java
@@ -0,0 +1,27 @@
+import java.util.HashSet;
+
+public class KthmissingSortedArrayDay35 {
+
+     public int kthMissing(int[] arr, int k) {
+        // code here
+        int cnt = 0;
+        HashSet<Integer> set = new HashSet<>();
+        
+        for(int i = 0; i < arr.length; i++){
+            set.add(arr[i]);
+        }
+        
+        for(int i = 1; i <= Integer.MAX_VALUE; i++){
+            if(!set.contains(i)){
+                ++cnt;
+                if(cnt == k){
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+    public static void main(String args[]){
+
+    }
+}
