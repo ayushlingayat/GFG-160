7f91102cf57409871dc9708c30d45995d7b4d783
Ayush Shyam Lingayat <lingayatayush3@gmail.com>
2025-03-03 11:25:20 +0530

    Queue_Deque

---
+++ Queue and Deque/SubarrayMaxDay108.java
@@ -0,0 +1,27 @@
+class Solution {
+    public ArrayList<Integer> maxOfSubarrays(int arr[], int k) {
+        ArrayList<Integer> result = new ArrayList<>();
+        Deque<Integer> deque = new LinkedList<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            // Remove elements out of the current window
+            while (!deque.isEmpty() && deque.peekFirst() < i - k + 1) {
+                deque.pollFirst();
+            }
+
+            // Remove elements smaller than the current element from the back
+            while (!deque.isEmpty() && arr[deque.peekLast()] < arr[i]) {
+                deque.pollLast();
+            }
+
+            // Add current element at the back of the deque
+            deque.offerLast(i);
+
+            // Store the max value for the window
+            if (i >= k - 1) {
+                result.add(arr[deque.peekFirst()]);
+            }
+        }
+        return result;
+    }
+}
